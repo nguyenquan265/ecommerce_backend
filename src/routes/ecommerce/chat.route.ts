9236308ae951110bfbb7396d9b44ac9d7734c0de
kimochiee <90102971+kimochiee@users.noreply.github.com>
2025-04-19 23:50:02 +0700

    updated chat route

--- src/routes/ecommerce/chat.route.ts
+++ src/routes/ecommerce/chat.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { getChatInformation } from '~/controllers/ecommerce/chat.controller'
+import { getChatInformation } from '../../controllers/ecommerce/chat.controller'
 
 const router = Router()
 
